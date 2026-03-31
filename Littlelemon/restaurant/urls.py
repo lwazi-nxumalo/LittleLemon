04083b89f7e6b588dc893686354252fe6fe976e5
Lwazi Nxumalo <134402686+lwazi-nxumalo@users.noreply.github.com>
2026-03-31 16:38:16 +0200

    update: index template sync

--- Littlelemon/restaurant/urls.py
+++ Littlelemon/restaurant/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from .views import index, MenuItemView, SingleMenuView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('menu/', MenuItemView.as_view(), name='menu'),
-    path('menu/<int:pk>/', SingleMenuView.as_view()),
+    path('', views.index, name='home'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', views.SingleMenuView.as_view()),
+    path('booking/', include(router.urls)), 
 ]
