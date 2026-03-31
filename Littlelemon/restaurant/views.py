7d1ad71b3a6dba02c8618d3c6eec753441d7edb3
Lwazi Nxumalo <134402686+lwazi-nxumalo@users.noreply.github.com>
2026-03-31 12:53:42 +0200

    feat: Views

--- Littlelemon/restaurant/views.py
+++ Littlelemon/restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from rest_framework import APIView
+from rest_framework.response import Response
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 # Create your views here.
 def index(request):
     return render(request, 'index.html' , {})
+
+class BookingView(APIView):
+    def get(self, request):
+        bookings = Booking.objects.all()
+        serializer = BookingSerializer(bookings, many=True)
+        return Response(serializer.data)
+    
+class MenuView(APIView):
+    def post(self, request):
+        serializer = MenuSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status" : "success" , "data" : serializer.data}, status=201)
+        return Response({"status" : "error" , "data" : serializer.errors}, status=400)
