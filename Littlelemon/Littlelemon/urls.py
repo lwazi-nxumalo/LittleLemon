d1eef48a30b2870c8a0ff9bfb9ab34b87bb786f2
Lwazi Nxumalo <134402686+lwazi-nxumalo@users.noreply.github.com>
2026-03-31 10:09:00 +0200

    add: Templates

--- Littlelemon/Littlelemon/urls.py
+++ Littlelemon/Littlelemon/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+#update URLConf by including URL patterns of restaurant app
 from django.contrib import admin
-from django.urls import path , include
-
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
+   path('admin/', admin.site.urls),
+   path('restaurant/', include('restaurant.urls')),
 ]
--- Littlelemon/restaurant/urls.py
+++ Littlelemon/restaurant/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path , include
+#define URL route for index() view
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index , name='index'),
+    path('', views.index, name='index')
 ]
