a4b84f38e07c5f9e7bc629bad9f604234747e437
Lwazi Nxumalo <134402686+lwazi-nxumalo@users.noreply.github.com>
2026-07-14 15:05:19 +0200

    change root url

--- Littlelemon/Littlelemon/urls.py
+++ Littlelemon/Littlelemon/urls.py
@@ -22,8 +22,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("", include("restaurant.urls")),
+    path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
